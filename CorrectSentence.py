f69504150c5862ff2969e172fd87971f4f7e5199
Rabia <96448337+Rsvnc@users.noreply.github.com>
2022-06-04 16:31:23 +0300

    Create CorrectSentence.py

---
+++ CorrectSentence.py
@@ -0,0 +1,27 @@
+# For the input of your function, you will be given one sentence. You have to return a corrected version, that starts with a capital letter and ends with a period (dot).
+
+# Pay attention to the fact that not all of the fixes are necessary. If a sentence already ends with a period (dot), then adding another one will be a mistake
+
+
+
+
+
+
+
+
+def correct_sentence(text: str) -> str:
+    return (text[0].upper()+text[1:].rstrip(".")+".")
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(correct_sentence("greetings, friends"))
+    
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert correct_sentence("greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends") == "Greetings, friends."
+    assert correct_sentence("Greetings, friends.") == "Greetings, friends."
+    assert correct_sentence("hi") == "Hi."
+    assert correct_sentence("welcome to New York") == "Welcome to New York."
+    
+    print("Coding complete? Click 'Check' to earn cool rewards!")
